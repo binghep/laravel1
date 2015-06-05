39cd63441fdd1a581d951c9896e95e85a28e6bca
Jiayi Peng <pengx077@gmail.com>
2015-06-05 11:37:45 -0700

    m

---
+++ app/Http/Controllers/Auth/mycontroller4.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Http\Controllers\Auth;
+
+
+class mycontroller4
+{
+
+    function j()
+    {
+        $i = 9;
+    }
+}
---
+++ app/Http/routes.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+//Route::get('/', 'WelcomeController@index');
+
+use Symfony\Component\HttpKernel\HttpCache\Store;
+
+class a
+{
+
+    /**
+     * @var Store
+     */
+    private $session;
+
+    public function __construct(Store $session)
+    {
+
+        $this->session = $session;
+    }
+}
+
+Route::get('/', function () {
+    return 'foo';
+});
+Route::get('/hello', function () {
+    return View::make(hello);
+});
+//Route::get('home', 'HomeController@index');
+
+Route::controllers([
+    'auth' => 'Auth\AuthController',
+    'password' => 'Auth\PasswordController',
+]);
