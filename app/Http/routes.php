7a9fc5c468973796882a30bab899c6065b384cc6
Jiayi Peng <pengx077@gmail.com>
2015-06-05 17:43:12 -0700

    m

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,35 +10,38 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
+//phpinfo();
 //Route::get('/', 'WelcomeController@index');
-
-use Symfony\Component\HttpKernel\HttpCache\Store;
-
-class a
-{
-
-    /**
-     * @var Store
-     */
-    private $session;
-
-    public function __construct(Store $session)
-    {
-
-        $this->session = $session;
-    }
-}
-
-Route::get('/', function () {
-    return 'foo';
-});
-Route::get('/hello', function () {
-    return View::make(hello);
-});
-//Route::get('home', 'HomeController@index');
-
-Route::controllers([
-    'auth' => 'Auth\AuthController',
-    'password' => 'Auth\PasswordController',
-]);
+echo 'heree';
+echo 'there';
+echo 'there';
+//use Symfony\Component\HttpKernel\HttpCache\Store;
+//
+//class a
+//{
+//
+//    /**
+//     * @var Store
+//     */
+//    private $session;
+//
+//    public function __construct(Store $session)
+//    {
+//
+//        $this->session = $session;
+//
+//    }
+//}
+//
+//Route::get('/', function () {
+//    return 'foo';
+//});
+//Route::get('/hello', function () {
+//    return View::make(hello);
+//});
+////Route::get('home', 'HomeController@index');
+//
+//Route::controllers([
+//    'auth' => 'Auth\AuthController',
+//    'password' => 'Auth\PasswordController',
+//]);
